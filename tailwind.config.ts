86a4d37dae5587a05a4e9f1499aabf0a8b2cfcfa
Jean-Paul Bassil <Jean.Paul.Bassil@OutLook.Com>
2024-07-13 14:42:55 +0300

    chore: add initial project files and configurations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,39 +1,40 @@
-import type { Config } from "tailwindcss";
+import { nextui } from '@nextui-org/theme'
+import type { Config } from 'tailwindcss'
 
-/** @type {import('tailwindcss').Config} */
-
-module.exports = {
-    darkMode: ["class"],
-    content: [
-        "./pages/**/*.{ts,tsx}",
-        "./components/**/*.{ts,tsx}",
-        "./app/**/*.{ts,tsx}",
-        "./src/**/*.{ts,tsx}",
-    ],
-    prefix: "",
-    theme: {
-        container: {
-            center: true,
-            padding: "2rem",
-            screens: {
-                sm: "640px",
-                md: "768px",
-                lg: "1024px",
-                xl: "1280px",
-                "2xl": "1536px",
-            },
-        },
-        extend: {
-            // our own custom box shadow
-            boxShadow: {
-                sm: "0 0 2px 0 rgba(0, 0, 0, 0.15)",
-            },
-            backgroundImage: {
-                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-                "gradient-conic":
-                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-            },
-        },
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
     },
-    plugins: [],
-};
+    colors: {
+    },
+    screens: {
+      xs: '320px',
+      sm: '375px',
+      md: '425px',
+      lg: '768px',
+      xl: '1024px',
+      '2xl': '1440px',
+      '3xl': '2560px',
+    },
+    transitionDuration: {
+      '1000': '1000ms',
+      '2000': '2000ms',
+      '3000': '3000ms',
+      '4000': '4000ms',
+      '5000': '5000ms',
+    },
+  },
+  darkMode: 'class',
+  plugins: [nextui()],
+}
+export default config
